20868f77fb5cbda3a2d38321d00521a35798fe81
Gökhan Taşkın <gokhanntaskinn@gmail.com>
2022-08-18 22:49:31 +0300

    User Model Updated

--- TestUygulama2/DataDB/User.cs
+++ TestUygulama2/DataDB/User.cs
@@ -11,5 +11,7 @@ namespace TestUygulama2.DataDB
         public string Name { get; set; }
         public string Surname { get; set; }
         public int? Age { get; set; }
+
+        public string Height { get; set; }
     }
 }
